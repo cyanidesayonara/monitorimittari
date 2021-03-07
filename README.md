b197aa0da891dac16a89df93f463cef1de41af0b
Santtu Nykänen <cyanidesayonara@gmail.com>
2021-03-07 23:52:30 +0200

    March 2021 update * Replaced openpyxl with editpyxl to enable saving in .xmlx format without corrupting file * Slight UI rearrangement * Bugfixes & refactoring * Updated dependencies

--- README.md
+++ README.md
@@ -1,48 +1,37 @@
 # Monitorimittari
-
 ## Install python 3
-
 https://www.python.org/downloads/
 
 ## Install and create virtualenv
-
-`pip install virtualenv`
-`virtualenv venv`
+`python -m venv venv`
 
 ## Activate virtualenv
-
-* On windows 
-  
- `venv/Scripts/activate`
-
-* On linux 
-  
- `source venv/Scripts/activate`
+* On windows  
+  `./venv/Scripts/activate`
+* On linux  
+  `source venv/bin/activate`
 
 ## Deactivate virtualenv
-
 `deactivate`
 
 ## Install requirements
-
 `pip install -r requirements.txt`
 
 ## Edit ui with pyqt5 designer
-
-`venv/Lib/site-packages/pyqt5_tools/designer.exe ui.ui`
+* On windows  
+  `venv/Lib/site-packages/pyqt5_tools/designer.exe ui.ui`
+* On linux  
+  `venv/Lib/site-packages/qt5_applications/Qt/bin/designer.exe ui.ui`
 
 ## Generate ui file
-
 `venv/Scripts/pyuic5.exe -x ui.ui -o ui.py`
 
 ## build and install exe file
-
 `python install.py`
 
-## PyQt5 tutorial
-
+## PyQt5 tutorials
+https://realpython.com/qt-designer-python/
 https://youtu.be/ksW59gYEl6Q
 
 ## Threading how-to
-
 https://www.mfitzp.com/article/multithreading-pyqt-applications-with-qthreadpool/
--- repository.py
+++ repository.py
@@ -3,88 +3,85 @@ import json
 from defaults import defaults
 from result import Result
 
-configFile = "config.json"
+CONFIG_FILE = "config.json"
 
 
 class Repository:
     def __init__(self):
-        if not os.path.isfile(configFile):
-            with open(configFile, "w+") as f:
-                f.write(json.dumps(defaults))
+        if not os.path.isfile(CONFIG_FILE):
+            with open(CONFIG_FILE, "w+") as file:
+                file.write(json.dumps(defaults, indent=2))
 
         # open and load config
-        with open(configFile) as f:
-
-            data = json.loads(f.read())
+        with open(CONFIG_FILE) as file:
+            data = json.loads(file.read())
             self.theme = data["theme"]
-            self.inputFile = data["inputFile"]
-            self.outputFile = ""
+            self.input_file = data["inputFile"]
+            self.output_file = ""
 
-            leftLNumber = data["left"]["lnumber"]
-            self.leftLNumber = Result(
-                name=leftLNumber["name"],
-                value=leftLNumber["value"],
-                cell=leftLNumber["cell"]
+            left_l_number = data["left"]["lnumber"]
+            self.left_l_number = Result(
+                name=left_l_number["name"],
+                value=left_l_number["value"],
+                cell=left_l_number["cell"]
             )
 
-            rightLNumber = data["right"]["lnumber"]
-            self.rightLNumber = Result(
-                name=rightLNumber["name"],
-                value=rightLNumber["value"],
-                cell=rightLNumber["cell"]
+            right_l_number = data["right"]["lnumber"]
+            self.right_l_number = Result(
+                name=right_l_number["name"],
+                value=right_l_number["value"],
+                cell=right_l_number["cell"]
             )
 
-            leftTester = data["left"]["tester"]
-            self.leftTester = Result(
-                name=leftTester["name"],
-                value=leftTester["value"],
-                cell=leftTester["cell"]
+            left_tester = data["left"]["tester"]
+            self.left_tester = Result(
+                name=left_tester["name"],
+                value=left_tester["value"],
+                cell=left_tester["cell"]
             )
 
-            rightTester = data["right"]["tester"]
-            self.rightTester = Result(
-                name=rightTester["name"],
-                value=rightTester["value"],
-                cell=rightTester["cell"]
+            right_tester = data["right"]["tester"]
+            self.right_tester = Result(
+                name=right_tester["name"],
+                value=right_tester["value"],
+                cell=right_tester["cell"]
             )
 
-            self.leftResults = []
+            self.left_results = []
             for result in list(data["left"]["results"]):
                 result = Result(
                     name=result["name"],
                     value=result["value"],
                     cell=result["cell"],
                 )
-                self.leftResults.append(result)
+                self.left_results.append(result)
 
-            self.rightResults = []
+            self.right_results = []
             for result in list(data["right"]["results"]):
                 result = Result(
                     name=result["name"],
                     value=result["value"],
                     cell=result["cell"],
                 )
-                self.rightResults.append(result)
+                self.right_results.append(result)
 
-            self.results = self.leftResults + self.rightResults
+            self.results = self.left_results + self.right_results
 
     def freeze(self):
-        configFile = "config.json"
-
         data = {
             "theme": self.theme,
-            "inputFile": self.inputFile,
+            "inputFile": self.input_file,
             "left": {
-                "lnumber": self.leftLNumber.__dict__,
-                "tester": self.leftTester.__dict__,
-                "results": [result.__dict__ for result in self.leftResults],
+                "lnumber": self.left_l_number.__dict__,
+                "tester": self.left_tester.__dict__,
+                "results": [result.__dict__ for result in self.left_results],
             },
             "right": {
-                "lnumber": self.rightLNumber.__dict__,
-                "tester": self.rightTester.__dict__,
-                "results": [result.__dict__ for result in self.rightResults],
+                "lnumber": self.right_l_number.__dict__,
+                "tester": self.right_tester.__dict__,
+                "results": [result.__dict__ for result in self.right_results],
             }
         }
 
-        with open(configFile, "w+") as f:
-            f.write(json.dumps(data))
+        with open(CONFIG_FILE, "w+") as file:
+            file.write(json.dumps(data, indent=2))
